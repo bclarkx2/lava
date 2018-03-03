0070f32c0766c824bca1d49242f061361831b599
Danny <djm230@case.edu>
2018-03-03 18:43:38 -0500

    fixed names
    Signed-off-by: Danny <djm230@case.edu>

--- src/interpreter.scm
+++ src/interpreter.scm
@@ -9,7 +9,7 @@
 
 (define interpret
   (lambda (filename)
-    (value (parser filename) (state.empty))))
+    (value (parser filename) (state-empty))))
 
 
 ;;; General helpers
@@ -33,16 +33,16 @@
            (value (cdr stmt-list) (state (car stmt-list) s))
            (value (car stmt-list) s)))
       (else
-       (value.evaluate stmt-list s)))))
+       (value-evaluate stmt-list s)))))
      
 ;Mathematical Operators
-(define value.int
+(define value-int
   (lambda (e s)
     (cond
       ((number? e) e)
       ((number? (car e)) (car e))
       ((and (eq? '- (operator e)) (unary? e))
-       (- 0 (value.evaluate(operand1 e s) s)))
+       (- 0 (value-evaluate(operand1 e s) s)))
       ((eq? '+ (operator e)) (compute + e s))
       ((eq? '- (operator e)) (compute - e s))
       ((eq? '* (operator e)) (compute * e s))
@@ -51,7 +51,7 @@
       (else (error 'badop "Undefined int operator")))))
 
 ;Comparison and Boolean Operations
-(define value.bool
+(define value-bool
   (lambda (e s)
     (cond
       ((eq? '== (operator e)) (compute eq? e s))
@@ -62,46 +62,47 @@
       ((eq? '<= (operator e)) (compute <= e s))
       ((eq? '&& (operator e)) (compute (lambda (a b) (and a b)) e s))
       ((eq? '|| (operator e)) (compute (lambda (a b) (or a b)) e s))
-      ((eq? '! (operator e)) (not (value.evaluate(operand1 e s) s)))
+      ((eq? '! (operator e)) (not (value-evaluate(operand1 e s) s)))
       (else (error 'badop "Undefined bool operator")))))
 
-(define value.evaluate
+(define value-evaluate
   (lambda (e s)
     (if (list? e)
         (cond
-          ((null? (cdr e)) (value.evaluate (car e) s))
-          ((interpreter-keyword? (operator e)) (value.statement e s))
-          ((int-operator? (operator e)) (value.int e s))
-          ((bool-operator? (operator e)) (value.bool e s))
+          ((null? (cdr e)) (value-evaluate (car e) s))
+          ((interpreter-keyword? (operator e)) (value-statement e s))
+          ((int-operator? (operator e)) (value-int e s))
+          ((bool-operator? (operator e)) (value-bool e s))
           (else (error 'badop "Undefined operator")))
         (cond
           ((number? e) e)
           ((boolean? e) e)
           ((eq? 'true e) #t)
           ((eq? 'false e) #f)
-          (else (state.lookup e s))))))
+          (else (state-lookup e s))))))
 
-(define value.statement
+(define value-statement
   (lambda (stmt s)
     (cond
-      ((eq? 'return (keyword stmt)) (value.return stmt s))
-      ((eq? 'if (keyword stmt)) (value.if stmt s))
+      ((eq? 'return (keyword stmt)) (value-return stmt s))
+      ((eq? 'if (keyword stmt)) (value-if stmt s))
+      ;((eq? 'begin (keyword stmt)) (value (cdr stmt) (addlayer(s))
       (else (null-value)))))
 
 ;has a value if the chosen clause has a return statement
-(define value.if
+(define value-if
   (lambda (stmt s)
-    (if (value.evaluate (condition stmt) s)
+    (if (value-evaluate (condition stmt) s)
         (value (stmt1 stmt) s)
         (value (stmt2 stmt) s))))
     
 
-(define value.return
+(define value-return
   (lambda (e s)
     (cond 
-      ((eq? (value.evaluate (cdr e) s) #t) 'true)
-      ((eq? (value.evaluate (cdr e) s) #f) 'false)
-      (else (value.evaluate (cdr e) s)))))
+      ((eq? (value-evaluate (cdr e) s) #t) 'true)
+      ((eq? (value-evaluate (cdr e) s) #f) 'false)
+      (else (value-evaluate (cdr e) s)))))
 
 
 ;abstractions for value
@@ -126,7 +127,7 @@
            (eq? 'true (cadr lis))
            (eq? 'false (cadr lis)))
            (cadr lis))
-      (else (state.lookup (cadr lis) s)))))
+      (else (state-lookup (cadr lis) s)))))
 
 (define operand2
   (lambda (lis s)
@@ -136,12 +137,12 @@
            (eq? 'true (caddr lis))
            (eq? 'false (caddr lis)))
            (caddr lis))
-      (else (state.lookup (caddr lis) s)))))
+      (else (state-lookup (caddr lis) s)))))
     
 (define compute
   (lambda (op e s)
-    (op (value.evaluate(operand1 e s) s)
-        (value.evaluate(operand2 e s) s))))
+    (op (value-evaluate(operand1 e s) s)
+        (value-evaluate(operand2 e s) s))))
 
 (define unary?
   (lambda (lis)
@@ -156,32 +157,32 @@
 
 ;;; Bindings
 
-(define state.empty (lambda () '(() ())))
+(define state-empty (lambda () '(() ())))
 (define null-value (lambda () '()))
 
-(define state.add-binding
+(define state-add-binding
   (lambda (var value s)
     (list (cons var (car s)) (cons value (cadr s)))))
      
 
-(define state.remove-binding
+(define state-remove-binding
   (lambda (var s)
     (cond
-      ((equal? s (state.empty)) (state.empty))
+      ((equal? s (state-empty)) (state-empty))
       ((eq? var (car (variables s))) (remaining s))
       (else (list
-             (cons (car (variables s)) (car (state.remove-binding var (remaining s))))
-             (cons (car (var-values s)) (cadr (state.remove-binding var (remaining s)))))))))
+             (cons (car (variables s)) (car (state-remove-binding var (remaining s))))
+             (cons (car (var-values s)) (cadr (state-remove-binding var (remaining s)))))))))
        
 
-(define state.lookup
+(define state-lookup
   (lambda (var s)
     (cond
       ((null? (variables s)) (raise 'illegal-var-dereferencing))
       ((eq? var (car (variables s)))
        (if (null? (car (var-values s))) (raise 'illegal-var-dereferencing)
            (car (var-values s))))
-       (else (state.lookup var (remaining s))))))
+       (else (state-lookup var (remaining s))))))
 
 (define variables
   (lambda (vartable) (car vartable)))
@@ -204,10 +205,10 @@
       ((eq? (keyword stmt) 'return) s)
 
       ; remaining operations delegated to helpers
-      ((eq? (keyword stmt) '=) (state.assign stmt s))
-      ((eq? (keyword stmt) 'if) (state.if stmt s))
-      ((eq? (keyword stmt) 'var) (state.var stmt s))
-      ((eq? (keyword stmt) 'while) (state.while stmt s))
+      ((eq? (keyword stmt) '=) (state-assign stmt s))
+      ((eq? (keyword stmt) 'if) (state-if stmt s))
+      ((eq? (keyword stmt) 'var) (state-var stmt s))
+      ((eq? (keyword stmt) 'while) (state-while stmt s))
 
       (else s))))
 
@@ -217,13 +218,13 @@
 
 ;; Assignment
 
-(define state.assign
+(define state-assign
   (lambda (stmt s)
     (if (in? (varname stmt) (variables s))
-    (state.add-binding
+    (state-add-binding
      (varname stmt)
-     (value.evaluate (varexpr stmt) s)  ;value of the expression
-     (state.remove-binding (varname stmt) s))
+     (value-evaluate (varexpr stmt) s)  ;value of the expression
+     (state-remove-binding (varname stmt) s))
     (raise 'assign-before-declare))))
 
 (define varname
@@ -236,9 +237,9 @@
 
 ;; If
 
-(define state.if
+(define state-if
   (lambda (stmt s)
-    (if (value.evaluate (condition stmt) s)
+    (if (value-evaluate (condition stmt) s)
         (state (stmt1 stmt) s)
         (state (stmt2 stmt) s))))
 
@@ -253,14 +254,14 @@
 
 ;; Var
 
-(define state.var
+(define state-var
   (lambda (stmt s)
     (if (in? (varname stmt) (variables s))
         (raise 'illegal-var-use)
         (if (has-initialization stmt)
-            (state.add-binding
-             (varname stmt) (value.evaluate (car (initialization stmt)) s) s)
-            (state.add-binding
+            (state-add-binding
+             (varname stmt) (value-evaluate (car (initialization stmt)) s) s)
+            (state-add-binding
              (varname stmt) '() s)))))
 
   (define has-initialization
@@ -272,9 +273,9 @@
 
   ;; While
 
-  (define state.while
+  (define state-while
     (lambda (stmt s) s
-      (if (value.evaluate (condition stmt) s)
+      (if (value-evaluate (condition stmt) s)
           (state stmt
                  (state (loopbody stmt)
                         (state (condition stmt)
