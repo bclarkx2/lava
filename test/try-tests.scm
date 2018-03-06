9aed110d1cb0a25331aa8e9ef8f61c1b889cbc96
Brian Clark <bclarkx2@gmail.com>
2018-03-06 16:20:03 -0500

    Add some failing tests for when there are returns in try and catch blocks -- is the finally block still supposed to run?

--- test/try-tests.scm
+++ test/try-tests.scm
@@ -50,7 +50,7 @@
    (assert-state
     '((var x 10)
       (try 
-       (begin
+       (begin 
         (= x 20)
         (throw 30))
       (catch (e)
@@ -58,3 +58,50 @@
       (finally
        (= x 40))))
     '((x) (40)))))
+
+; return in try w/ finally
+(define test-1-5
+ (lambda ()
+  (assert-state
+   '((var x 10)
+     (try
+      (begin
+       (= x 20
+       (return x)))
+     ()  
+     (finally
+      (= x 30))))
+   '((x) (30)))))
+
+; try-finally inside while loop; break inside try
+(define test-2-1
+ (lambda ()
+  (assert-state
+   '((var x 10)
+     (while (> x 0)
+     (begin
+      (= x (- x 1))
+      (try
+       (break)
+      ()  
+      (finally
+       (= x 99))))))
+   '((x) (99)))))
+
+; try-finally in while; break in catch
+(define test-2-2
+ (lambda ()
+  (assert-state
+   '((var x 10)
+     (while (> x 0)
+     (begin
+      (= x (- x 1))
+      (try
+       (throw 20)
+      (catch (e)
+       (begin
+        (= x e)
+        (break)))
+      (finally
+       (= x 99))))))
+   '((x) (99)))))
