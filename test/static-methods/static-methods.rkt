dc7c9db83f912bbf5ebf3e302f7c155214ed95a6
Brian Clark <bclarkx2@gmail.com>
2018-11-03 22:30:42 -0500

    Added basic static method test

---
+++ test/static-methods/static-methods.rkt
@@ -0,0 +1,17 @@
+
+#lang racket
+(provide (all-defined-out))
+
+;;; requirements
+(require xrepl)
+(require racket/trace)
+(require rackunit "../../src/interpreter.rkt")
+(require "../common.rkt")
+
+
+;;; test cases
+
+(assert (test-interpret-class "basic-static-method" "A")
+        3
+        "basic-static-method")
+
